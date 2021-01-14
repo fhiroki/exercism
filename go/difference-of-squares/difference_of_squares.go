b3c57f0134ec023d3ae65f2e349a3f02dc5ebcee
fhiroki <hiroki976@gmail.com>
2021-01-14 11:46:52 +0900

    solve go/difference-of-squares

---
+++ go/difference-of-squares/difference_of_squares.go
@@ -0,0 +1,17 @@
+package diffsquares
+
+// SquareOfSum returns the square of the sum.
+func SquareOfSum(n int) int {
+	sum := n * (n + 1) / 2
+	return sum * sum
+}
+
+// SumOfSquares returns the sum of the squares.
+func SumOfSquares(n int) int {
+	return n * (n + 1) * (2*n + 1) / 6
+}
+
+// Difference returns diff between the square of the sum and the sum of the squares.
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
