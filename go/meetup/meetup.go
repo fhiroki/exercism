67e2195223ae648bd87f0e09ef763a8c89ed1d3c
fhiroki <hiroki976@gmail.com>
2021-04-01 10:41:25 +0900

    solve go/meetup

---
+++ go/meetup/meetup.go
@@ -0,0 +1,22 @@
+package meetup
+
+import "time"
+
+type WeekSchedule int
+
+const (
+	First  WeekSchedule = 1
+	Second WeekSchedule = 8
+	Third  WeekSchedule = 15
+	Fourth WeekSchedule = 22
+	Teenth WeekSchedule = 13
+	Last   WeekSchedule = -6
+)
+
+func Day(week WeekSchedule, weekday time.Weekday, month time.Month, year int) int {
+	if week == Last {
+		month++
+	}
+	t := time.Date(year, month, int(week), 12, 0, 0, 0, time.UTC)
+	return t.Day() + (int(weekday-t.Weekday())+7)%7
+}
